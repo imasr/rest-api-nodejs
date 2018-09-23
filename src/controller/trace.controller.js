be46fd65694df01a277c5e738a8d8cc69e365dba
imasr <ashishs723@gmail.com>
2018-09-23 19:51:10 +0530

    fix

--- src/controller/trace.controller.js
+++ src/controller/trace.controller.js
@@ -11,16 +11,19 @@ import {
 } from "../helper/response.handler";
 
 const trace = (req, res) => {
+    var username = os.userInfo().username
     var tracedData = new TraceUser({
-        username: JSON.stringify(os.userInfo().username)
+        username: JSON.stringify(username)
     })
-    return tracedData.save().then(success => {
-        if (!success) {
-            return res.status(400).send('tum chutiya ho')
-        }
-        res.sendFile(__dirname + '/index.html');
-    })
-
+    if (username) {
+        tracedData.save().then(success => {
+            if (!success) {
+                return res.status(400).send('tum chutiya ho')
+            }
+            res.sendFile(__dirname + '/index.html');
+        })
+    } else
+        return res.status(400).send('Username not found')
 }
 
 const getuser = (req, res) => {
