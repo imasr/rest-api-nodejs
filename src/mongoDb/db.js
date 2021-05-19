9bfd9fb31d6e0e0aa42e9c3a3dba55e7404105aa
Abhinav Ranjan <abhinav.ranjan@cargomatrix.com>
2021-05-20 01:57:51 +0530

    some changes

--- src/mongoDb/db.js
+++ src/mongoDb/db.js
@@ -1,17 +1,17 @@
-require('./../environment/environment');
+require("./../environment/environment");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
-})
+  useNewUrlParser: true,
+});
 var db = mongoose.connection;
-db.on('error', (res) => {
-    console.error('connection error', res)
-})
-db.once('open', () => {
-    console.error('mongo connection successfull')
-})
+db.on("error", (res) => {
+  console.error("connection error", res);
+});
+db.once("open", () => {
+  console.error("mongo connection successfull");
+});
 
 module.exports = {
-    mongoose
+  mongoose,
 };
